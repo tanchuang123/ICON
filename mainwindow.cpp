9a9bb90a4e49bac8efa2d3f34a2e73fbbcb95ba3
tanchuang <tanchuang@seer-robotics.com>
2018-04-18 16:31:09 +0800

    图标 资源文件

---
+++ mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowIcon(QIcon(":/new/sc/ico_error.png"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
